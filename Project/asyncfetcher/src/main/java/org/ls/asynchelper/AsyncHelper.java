091772d21fe02e6fee3058457301d1ad42a9ec75
Loganathan S <loganathans@ilink-systems.com>
2017-08-21 15:56:35 +0530

    Minor code refactoring.

--- Project/asyncfetcher/src/main/java/org/ls/asynchelper/AsyncHelper.java
+++ Project/asyncfetcher/src/main/java/org/ls/asynchelper/AsyncHelper.java
@@ -255,7 +255,7 @@ public enum AsyncHelper {
 			}
 
 			@Override
-			public Void invokeNextTask() {
+			public Void invokeNextFunction() {
 				runnables[index.getAndIncrement()].run();
 				return null;
 			}
@@ -299,7 +299,7 @@ public enum AsyncHelper {
 			}
 
 			@Override
-			public Void invokeNextTask() {
+			public Void invokeNextFunction() {
 				if(index.get() == runnables.length) {
 					//Cycle again
 					index.set(0);
@@ -350,7 +350,7 @@ public enum AsyncHelper {
 			public void run() {
 				synchronized (schedulingFunction) {
 					if (schedulingFunction.canRun()) {
-						R res = schedulingFunction.invokeNextTask();
+						R res = schedulingFunction.invokeNextFunction();
 						schedulingFunction.consumeResult(res);
 						if (schedulingFunction.canCancel()) {
 							if (scheduleFuture[0] != null) {
@@ -490,7 +490,7 @@ public enum AsyncHelper {
 			}
 
 			@Override
-			public T invokeNextTask() {
+			public T invokeNextFunction() {
 				return suppliers[index.getAndIncrement()].get();
 			}
 
@@ -565,7 +565,7 @@ public enum AsyncHelper {
 			}
 
 			@Override
-			public T invokeNextTask() {
+			public T invokeNextFunction() {
 				if(index.get() == suppliers.length) {
 					//Cycle again
 					index.set(0);
@@ -1068,7 +1068,7 @@ public enum AsyncHelper {
 	}
 
 	/**
-	 * The Interface SchedulingTask.
+	 * The Interface SchedulingFunction.
 	 *
 	 * @param <T> the generic type
 	 * @param <R> the generic type
@@ -1090,11 +1090,11 @@ public enum AsyncHelper {
 		boolean canCancel();
 		
 		/**
-		 * Invoke next task.
+		 * Invoke next function.
 		 *
 		 * @return the r
 		 */
-		R invokeNextTask();
+		R invokeNextFunction();
 		
 		/**
 		 * Consume result.
