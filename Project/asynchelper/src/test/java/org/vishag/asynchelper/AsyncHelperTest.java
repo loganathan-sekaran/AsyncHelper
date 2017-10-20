d822e87829942f42daf1f28294f4b1b867a510b7
Loganathan S <loganathans@ilink-systems.com>
2017-10-20 15:39:18 +0530

    Fix for stalling test.

--- Project/asynchelper/src/test/java/org/vishag/asynchelper/AsyncHelperTest.java
+++ Project/asynchelper/src/test/java/org/vishag/asynchelper/AsyncHelperTest.java
@@ -753,7 +753,7 @@ public class AsyncHelperTest {
 		AsyncHelper.submitTask(() -> {
 			System.out.println("Inside notification task.");
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
