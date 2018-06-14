b4e467d3708d352564e491ff584fdaadd74b8310
Loganathan S <loganathan001@gmail.com>
2018-06-14 22:46:25 +0530

    More fix in travis

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true jacoco:report coveralls:report
     exit $?
 fi
