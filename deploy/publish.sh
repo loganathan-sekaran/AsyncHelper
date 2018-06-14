572a72c3cb6d706b5ee100adcc432627fff5dea1
Loganathan S <loganathan001@gmail.com>
2018-06-14 22:34:38 +0530

    Minor fix in travis.

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-	cd Project/asynchelper
     mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
     exit $?
 fi
