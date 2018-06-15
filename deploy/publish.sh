a2e6aa49164df41ac3f1e915dee3266fb4191588
Loganathan S <loganathan001@gmail.com>
2018-06-15 15:42:42 +0530

    using different gpg approach

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy jacoco:report coveralls:report -X --settings $GPG_DIR/settings.xml -DperformRelease=true
+    mvn deploy -X --settings $GPG_DIR/settings.xml -DperformRelease=true
     exit $?
 fi
