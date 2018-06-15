a35e76aa1a232401b43eff82b0526986bc1388fb
Loganathan S <loganathan001@gmail.com>
2018-06-15 16:04:13 +0530

    Changes to travis build.

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy -X --settings $GPG_DIR/settings.xml -DperformRelease=true
+    mvn deploy jacoco:report coveralls:report --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -B
     exit $?
 fi
